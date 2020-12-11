ebe9e7f3a48f22a277dc8a9c981b4fe2e2b1c701
Jhon Cervantes <jhonfredycercha@hotmail.com>
2020-12-10 19:24:47 -0500

    Agregando Link to a header

--- src/components/headerAvatar.js
+++ src/components/headerAvatar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import Avatar from './avatar';
 
@@ -12,19 +13,19 @@ class HeaderAvatar extends React.Component{
             <div className="row">
                 <div className="col-12">
                     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <a className="navbar-brand" href="/">Monitoreo</a>
+                        <Link className="navbar-brand" to="/">Monitoreo</Link>
 
                         <div className="collapse navbar-collapse" id="navbarNavDropdown">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/ingreso">Ingreso</a>
+                                    <Link className="nav-link" to="/ingreso">Ingreso</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/map">Mapa</a>
+                                    <Link className="nav-link" to="/map">Mapa</Link>
                                 </li>
                             </ul>
                         </div>
-                        <Avatar></Avatar>
+                        <Avatar />
                     </nav>
                 </div>
             </div>
